80b2203c3c406d89e496b63c44d1c0d13ae7e111
Igor Nebeschenko <nebeschenkoichor@gmail.com>
2017-09-27 15:45:59 +0300

    get row index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
   var table = document.querySelector(".container__table");
   table.addEventListener("mouseover", function( event ) {
-    console.log('row ' + event.target.rowIndex, 'cell ' + event.target.cellIndex);
+    console.log('cell ' + event.target.cellIndex);
   })
+  let rows = document.querySelectorAll(".container__table tr");
+  for(let x = 0, xLength = rows.length; x < xLength; x++) {
+    console.log('rowIndex= ' + rows[x].rowIndex);
+  }
 });
