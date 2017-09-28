aa7bf3f361c2e07999d520b069fda135582fd178
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2017-09-28 09:38:06 +0300

    express server

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/style/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname+'/style/style.css'));
+});
+
+app.get('/scripts/index.js', function (req, res) {
+  res.sendFile(path.join(__dirname+'/scripts/index.js'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
